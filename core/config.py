a3b1cb393eb0cc6ac5f11577908537bebd01afa1
Valentin Belyakov <devil1546@yandex.ru>
2024-04-25 13:51:11 +0300

    update SettingDB

--- core/config.py
+++ core/config.py
@@ -1,16 +1,39 @@
+import pydantic as pd
+import typing as tp
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class SettingsDB(BaseSettings):
-    model_config = SettingsConfigDict(env_file=".env")
+    model_config = SettingsConfigDict()
 
-    POSTGRESQL_HOST: str
-    POSTGRESQL_PORT: str
-    POSTGRESQL_USER: str
-    POSTGRESQL_PASSWORD: str
-    POSTGRESQL_DATABASE: str
-    POSTGRESQL_SCHEMA: str
+    POSTGRESQL_HOST: tp.Optional[str] = None
+    POSTGRESQL_PORT: tp.Optional[int] = None
+    POSTGRESQL_USER: tp.Optional[str] = None
+    POSTGRESQL_PASSWORD: tp.Optional[str] = None
+    POSTGRESQL_DATABASE: tp.Optional[str] = None
+    POSTGRESQL_SCHEMA: tp.Optional[str] = None
+    DATABASE_URI: tp.Union[tp.Optional[pd.PostgresDsn], tp.Optional[str]] = None
+
+    @pd.field_validator("DATABASE_URI", mode="before")
+    @classmethod
+    def assemble_db_connection(
+            cls,
+            value: tp.Optional[str],
+            values: pd.ValidationInfo
+    ) -> tp.Any:
+        if isinstance(value, str):
+            return value
+        return pd.PostgresDsn.build(
+            scheme="postgresql",
+            username=values.data.get("POSTGRESQL_USER"),
+            password=values.data.get("POSTGRESQL_PASSWORD"),
+            host=values.data.get("POSTGRESQL_HOST"),
+            port=values.data.get("POSTGRESQL_PORT"),
+            path=f"{values.data.get('POSTGRESQL_DATABASE') or ''}",
+        )
 
 
 settings_db = SettingsDB(_env_file="../.env")
-print(settings_db)
+settings_db_test = SettingsDB(_env_file="../test_db.env")
+print(f"{settings_db.DATABASE_URI.unicode_string()}")
+print(f"{settings_db_test.DATABASE_URI.unicode_string()}")
